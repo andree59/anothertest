3fbac69951d4bc3c88e6690697034b635195b26f
andree_peyrot <andreepeyrot@att.net>
2017-04-20 13:58:45 -0400

    added function to app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+let myfavorites = {
+  a:'landscaping gardens',
+  b:'working on house projects',
+  c:'helping others with their house projects'
+}
+
+for(let item in myfavorites){
+  console.log(myfavorites[item])
+}
